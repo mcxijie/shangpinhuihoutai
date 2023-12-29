bfe60e378121956e036a7219a28d9d0bc3a05443
xijie <907725402@qq.com>
2023-12-29 14:55:06 +0800

    1.权限管理模块

--- src/api/index.js
+++ src/api/index.js
@@ -3,9 +3,17 @@ import * as attr from "./product/attr";
 import * as sku from "./product/sku";
 import * as spu from "./product/spu";
 
+import * as user from "./acl/user";
+import role from "@/api/acl/role";
+import permission from "@/api/acl/permission";
+
+
 export default {
   tradeMark,
   attr,
   sku,
-  spu
+  spu,
+  user,
+  role,
+  permission
 }
