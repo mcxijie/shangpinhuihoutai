21f866e6332fc67d21fa4668dfccc6841cbd3f08
xijie <907725402@qq.com>
2023-12-29 13:15:09 +0800

    1.Sale静态组件 2.Sale组件完毕 3.observe静态组件

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
     <Card></Card>
+    <Sale></Sale>
+    <Observe></Observe>
   </div>
 </template>
 
 <script>
 import Card from './Card/index.vue';
+import Sale from "./Sale/index.vue";
+import Observe from "@/views/dashboard/Observe/index.vue";
 
 export default {
   name: 'Dashboard',
   components: {
     Card,
+    Sale,
+    Observe,
   },
 }
 </script>
